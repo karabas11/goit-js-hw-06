26051b42e3f5996f004e81065435b9905ff358a0
German Sokolovskyy <101786762+karabas11@users.noreply.github.com>
2023-07-02 12:52:25 +0300

    add hw 02
    add hw 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const list = document.querySelector('#ingredients');
+
+const item = ingredients.map(item => {
+  const li = document.createElement('li');
+  li.textContent = item;
+  li.classList.add('item');
+  // console.log(li);
+  // console.log(item);
+  return li;
+});
+
+const liRef = list.append(...item);
+console.log(liRef);
