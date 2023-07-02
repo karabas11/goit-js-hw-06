323f2b3c29aa454695188b505f86bc3a32da4249
German Sokolovskyy <101786762+karabas11@users.noreply.github.com>
2023-07-02 13:07:22 +0300

    add hw 10
    add hw 10
    OH! END of HW JS 06!!!

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
+const amountInput = document.querySelector('input');
+console.log(amountInput.value);
+const creatBtnRef = document.querySelector('[data-create]');
+const destroyBtnRef = document.querySelector('[data-destroy]');
+const boxesRef = document.querySelector('#boxes');
+
+creatBtnRef.addEventListener('click', onCreat);
+destroyBtnRef.addEventListener('click', onDestroy);
+
+function createBoxes(amount) {
+  let container = [];
+  for (let i = 0; i <= amount; i += 1) {
+    const box = document.createElement('div');
+
+    box.style.width = 30 + 10 * i + 'px';
+    box.style.height = 30 + 10 * i + 'px';
+    box.style.backgroundColor = getRandomHexColor();
+    container.push(box);
+  }
+  console.log(container);
+  boxesRef.append(...container);
+}
+
+function onCreat() {
+  const amount = amountInput.value;
+
+  if (amount > 0) {
+    createBoxes(amount);
+  }
+}
+
+function onDestroy() {
+  boxesRef.innerHTML = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
