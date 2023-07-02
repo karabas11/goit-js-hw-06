b9ef11d3c2975aea86c702f5373ffc23833b796e
German Sokolovskyy <101786762+karabas11@users.noreply.github.com>
2023-07-02 13:02:48 +0300

    add hw 08
    add hw 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Поля повинні бути заповнені');
+  }
+
+  const data = {
+    emailUser: email.value,
+    passwordUser: password.value,
+  };
+  console.log(data);
+  event.currentTarget.reset();
+}
