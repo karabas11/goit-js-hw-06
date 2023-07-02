2eb9693ed8a09bcb4278b557c43d5ad9b4efac11
German Sokolovskyy <101786762+karabas11@users.noreply.github.com>
2023-07-02 13:04:39 +0300

    add hw 09
    add hw 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const conteiner = document.querySelector('.widget');
+const btnChangeColor = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+btnChangeColor.addEventListener('click', onChangeColor);
+
+function onChangeColor(event) {
+  conteiner.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
